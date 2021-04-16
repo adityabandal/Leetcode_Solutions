c1f983224da64ef9a72d93ca5eae0580b507adc6
Aditya Bandal <42375666+adityabandal@users.noreply.github.com>
2021-04-16 11:24:40 +0530

    Time: 16 ms (49.52%), Space: 17.3 MB (94.22%) - LeetHub

---
+++ toeplitz-matrix/toeplitz-matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(mat[i][j] != mat[i-1][j-1]) return false;
+            }
+        }
+        
+        return true;
+    }
+};
