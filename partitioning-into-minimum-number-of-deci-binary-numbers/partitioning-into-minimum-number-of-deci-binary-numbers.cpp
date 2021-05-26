4f48a63c4d2e1d29c2074989909cd1dbb94d5866
Aditya Bandal <42375666+adityabandal@users.noreply.github.com>
2021-05-26 12:41:08 +0530

    Time: 36 ms (46.90%), Space: 13.6 MB (29.40%) - LeetHub

---
+++ partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        int mx = 0;
+        for(char c:n){
+            mx = max(mx, c-'0');
+        }
+        return mx;
+    }
+};
