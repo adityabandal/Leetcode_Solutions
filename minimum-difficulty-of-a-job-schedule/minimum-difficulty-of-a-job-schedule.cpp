c6e2d9e0b73ec86d101f6410068e248597248581
Aditya Bandal <42375666+adityabandal@users.noreply.github.com>
2021-05-28 21:30:41 +0530

    Time: 16 ms (93.59%), Space: 7.1 MB (96.33%) - LeetHub

--- minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
+++ minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
@@ -3,26 +3,22 @@ public:
     int dp[11][300];
     int help(vector<int>& jd, int d, int last){
         if(d>last+1) return -1;
-        if(dp[d][last]!=-1) return dp[d][last];
-        if(d==1){
-            int ans = 0;
-            for(int i=0;i<=last;i++)
-                ans = max(ans, jd[i]);
-            return dp[d][last]=ans;
-        }        
-        else{
-            int mx = 0;
-            int ans = INT_MAX;
-            
-            for(int i=last;i>=0;i--){
-                mx = max(mx, jd[i]);
-                int nxt = help(jd, d-1, i-1);
-                if(nxt!=-1){
-                    ans = min(ans, nxt+mx);
-                }
+        if(last == -1 && d == 0) return 0;
+        if(last == -1 || d == 0) return -1;
+        if(dp[d][last] != -1) return dp[d][last];
+        
+        int mx = 0, ans = INT_MAX;
+        
+        for(int i=last;i>=d-1;i--){
+            mx = max(mx, jd[i]);
+            int nxt = help(jd, d-1, i-1);
+            if(nxt!=-1){
+                ans = min(ans, nxt+mx);
             }
-            return dp[d][last]=ans;
         }
+        
+        return dp[d][last] = ans;
+        
     }
     int minDifficulty(vector<int>& jd, int d) {
         if(d>jd.size()) return -1;
