294f318e916189c74145d97c0908b2906f8638b0
Aditya Bandal <42375666+adityabandal@users.noreply.github.com>
2021-06-15 12:28:05 +0530

    Time: 16 ms (91.26%), Space: 7.1 MB (78.38%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -3,18 +3,17 @@ public:
     string longestPalindrome(string s) {
         int n = s.size();
         
-        int dp[n][n];
-        memset(dp, -1, sizeof(dp));
-        
-        string ans = "";
+        int start = -1;
+        int mx = 0;
         
         for(int i=0;i<n;i++){
             int lo = i;
             int hi = i;
             
             while(lo>=0 && hi<n && s[lo]==s[hi]){
-                if(hi-lo+1>ans.size()){
-                    ans = s.substr(lo, hi-lo+1);
+                if(hi-lo+1>mx){
+                    start = lo;
+                    mx = hi-lo+1;
                 }
                 lo--;
                 hi++;
@@ -24,28 +23,16 @@ public:
             hi = i+1;
             
             while(lo>=0 && hi<n && s[lo]==s[hi]){
-                if(hi-lo+1>ans.size()){
-                    ans = s.substr(lo, hi-lo+1);
+                if(hi-lo+1>mx){
+                    start = lo;
+                    mx = hi-lo+1;
                 }
                 lo--;
                 hi++;
             }
         }
+                
         
-//         for(int i=0;i<n-1;i++){
-//             int lo = i;
-//             int hi = i+1;
-            
-//             while(lo>=0 && hi<n && s[lo]==s[hi]){
-//                 if(hi-lo+1>ans.size()){
-//                     ans = s.substr(lo, hi-lo+1);
-//                 }
-//                 lo--;
-//                 hi++;
-//             }
-//         }
-        
-        
-        return ans;
+        return s.substr(start, mx);
     }
 };
